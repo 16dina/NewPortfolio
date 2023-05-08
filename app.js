65f1f578145e7b0550b08a5004bdf63ad8fc6a9f
Dina Boshnaq <r0823887@student.thomasmore.be>
2023-05-08 02:26:05 +0200

    split into pages for faster js loading

--- app.js
+++ app.js
@@ -6,34 +6,34 @@ const allSections = document.querySelector('.main-content');
 function PageTransitions(){
     // Button click active class
     // Note: "this" keyword doesn't exist in an arrow function
-    for(let i = 0; i < sectBtn.length; i++){
-        sectBtn[i].addEventListener('click', function(){
-            let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            // Make sure you have a space before "active-btn" in the following line
-            this.className += ' active-btn';
-        })
-    }
+    // for(let i = 0; i < sectBtn.length; i++){
+    //     sectBtn[i].addEventListener('click', function(){
+    //         let currentBtn = document.querySelectorAll('.active-btn');
+    //         currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+    //         // Make sure you have a space before "active-btn" in the following line
+    //         this.className += ' active-btn';
+    //     })
+    // }
 
     // Sections Active class
-    allSections.addEventListener('click', (e) =>{
-        const id = e.target.dataset.id;
-        if(id){
-            // Remove selected from the other btns
-            sectBtns.forEach((btn) =>{
-                btn.classList.remove('active')
-            })
-            e.target.classList.add('active')
+    // allSections.addEventListener('click', (e) =>{
+    //     const id = e.target.dataset.id;
+    //     if(id){
+    //         // Remove selected from the other btns
+    //         sectBtns.forEach((btn) =>{
+    //             btn.classList.remove('active')
+    //         })
+    //         e.target.classList.add('active')
             
-            // Hide other sections
-            sections.forEach((section) =>{
-                section.classList.remove('active')
-            })
+    //         // Hide other sections
+    //         sections.forEach((section) =>{
+    //             section.classList.remove('active')
+    //         })
 
-            const element = document.getElementById(id);
-            element.classList.add('active');
-        }
-    })
+    //         const element = document.getElementById(id);
+    //         element.classList.add('active');
+    //     }
+    // })
 
     // Toggle theme
     const themeBtn = document.querySelector('.theme-btn');
